022567ec72072a4e4c4654c711b27a13530895ae
Isanjalee Silva <silvaaihn.19@uom.lk>
2022-06-13 02:52:02 +0530

    Earnings #3

--- server/routes/earnings.js
+++ server/routes/earnings.js
@@ -1,5 +1,49 @@
-const mongoose = require("mongoose");
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-module.exports = router;
+//Imports
+const Earnings = require('../models/earnings');//Folder Model
+const ResponseService = require('../utils/ResponsesService'); // Response service
+
+// Create
+router.post("/", async (req, res) => {
+    new Earnings(req.body).save((err, doc) => {
+        ResponseService.generalPayloadResponse(err, doc, res);
+    });
+});
+
+//get all
+router.get('/', (req, res) => {
+    Earnings.find((err, doc) => {
+        ResponseService.generalPayloadResponse(err, newPayload, res);
+    })
+        .sort({ addedOn: -1 })
+        .populate('addedBy', 'firstName lastName')
+});
+
+// // Update
+// router.put("/:id", async (req, res) => {
+    
+//     Earnings.findByIdAndUpdate(req.body.id, req.body, (err, doc) => {
+//         ResponseService.generalPayloadResponse(err, doc, res, "Updated");
+//     });
+// });
+
+// // Get by id
+// router.get('/:id', (req, res) => {
+//     Earnings.findById(req.params.id, (err, doc) => {
+//         ResponseService.generalPayloadResponse(err, doc, res);
+//     });
+// });
+
+// // Delete
+// router.delete('/:id', (req, res) => {
+//     Earnings.findByIdAndRemove(req.body.id, (err, doc) => {
+//         ResponseService.generalResponse(err, res, "task removed successfully");
+//     });
+// });
+
+
+
+
+module.exports=router;
