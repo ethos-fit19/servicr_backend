f404362c14cae4967ae675a27a1d458454361a78
sachla <sach.pushpawela@gmail.com>
2022-06-12 19:21:10 +0530

    Added validation

--- server/models/user.js
+++ server/models/user.js
@@ -17,6 +17,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  bio: {
+    type: String,
+  },
 });
 
 mongoose.model("User", userSchema);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const User = mongoose.model("User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const requireLogin = require("../middleware/requireLogin");
 
+const User = mongoose.model("User");
+
+//User registration
 router.post("/register", (req, res) => {
-  const { name, userType, email, password } = req.body;
-  if (!name || !userType || !email || !password) {
+  const { name, userType, email, password, confirmPassword } = req.body;
+  //Checking empty fields
+  if (!name || !userType || !email || !password || !confirmPassword) {
     res.status(422).json({ error: "Please fill all the fields" });
   }
+
+  //Checking the email format
+  if (
+    !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email
+    )
+  ) {
+    return res.status(422).json({ error: "Invalid email format!" });
+  }
+
+  //Checking password mismatch
+  if (password != conPassword) {
+    return res.status(422).json({ error: "Passwords mismatch!" });
+  }
+  //Checking user
   User.findOne({ email: email })
     .then((savedUser) => {
       if (savedUser) {
@@ -18,6 +36,7 @@ router.post("/register", (req, res) => {
           .status(422)
           .json({ error: "user already exists with that email" });
       }
+      //Hashing password
       bcrypt.hash(password, 12).then((hashedpassword) => {
         const user = new User({
           name,
@@ -41,11 +60,24 @@ router.post("/register", (req, res) => {
     });
 });
 
+//User login
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
+  //Checking Empty Fields
   if (!email || !password) {
-    return res.status(422).json({ error: "Please add email and password" });
+    return res.status(422).json({ error: "Please enter all the fields!" });
   }
+
+  //Checking the email format
+  if (
+    !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email
+    )
+  ) {
+    return res.status(422).json({ error: "Invalid email format!" });
+  }
+
+  // Matching entered email with database
   User.findOne({ email: email }).then((savedUser) => {
     if (!savedUser) {
       return res.status(422).json({ error: "Invalid Email or password" });
