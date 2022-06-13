ba61a40e884db4b9153f8208af3587411779d4d4
Udara <nanayakkaranpdui.19@uom.lk>
2022-06-13 14:45:15 +0530

    reviews#2

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -1,39 +1,24 @@
 const mongoose = require("mongoose");
 
 const reviewsSchema = new mongoose.Schema({
-    customerId: {
-        type:mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    serviceProviderId: {
-        type:mongoose.Schema.Types.ObjectId,
-        ref: "ServiceProvider",
-      },
-    serviceCtegory:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"ServiceCategory"
+    starRating: {
+        type: String,
     },
-    reviewId:{
-        type:String,
-        req:true
+    review: {
+        type: String,
     },
-    starRating:{
-        type:String,
+    AddedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
     },
-comments:{
-    type:String,
- },
- from : {
-    type : mongoose.Schema.Types.ObjectId,
-    ref : 'User',
-},
-to : {  // recieved review from another people
-    type : mongoose.Schema.Types.ObjectId,
-    ref : 'User',
-}
+    Servicer: {  // recieved review from another people
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'serviceProvider',
+    },
+    addedOn:{
+        type: Date,
+    },
 
-},{
-timestamps : true,
 });
 
 mongoose.model("Reviews", reviewsSchema);
