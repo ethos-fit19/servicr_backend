795aff41e035efa5b028bc1bbba5d35816e6eb18
Udara <nanayakkaranpdui.19@uom.lk>
2022-06-13 12:46:10 +0530

    Reviews #1

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ require("./models/service");
 require("./models/appointment");
 require("./models/earnings");
 require("./models/serviceProvider");
+require("./models/reviews");
 
 // routes
 const authRoutes = require("./routes/auth");
@@ -28,12 +29,16 @@ const serviceCategories = require("./routes/serviceCategories");
 const serviceRoutes = require("./routes/service");
 const appointmentRoutes = require("./routes/appointment");
 const earningsRoutes = require("./routes/earnings");
+const reviewsRoutes = require("./routes/reviews");
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", serviceCategories);
 app.use("/api/services", serviceRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/earnings", earningsRoutes);
+app.use("/api/reviews", reviewsRoutes);
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
