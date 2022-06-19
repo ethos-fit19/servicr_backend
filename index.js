400fe16ca2f5c5cd554c3f75696ac42f5d66f22a
sachla <sach.pushpawela@gmail.com>
2022-06-19 18:32:37 +0530

    Added heroku proc file

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ const serviceRoutes = require("./routes/service");
 const appointmentRoutes = require("./routes/appointment");
 const reviewsRoutes = require("./routes/reviews");
 const serviceProviderRoutes = require("./routes/serviceProvider");
-const usersRoutes = require("./routes/users");
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -43,7 +42,6 @@ app.use("/api/services", serviceRoutes);
 app.use("/api/appointments", appointmentRoutes);
 app.use("/api/reviews", reviewsRoutes);
 app.use("/api/serviceProvider", serviceProviderRoutes);
-app.use("/api/users", usersRoutes);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
