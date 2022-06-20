275ba2f9299d33bedaf1b2d52a6c0db6634f62e2
sachla <sach.pushpawela@gmail.com>
2022-06-20 23:14:32 +0530

    user model fix

--- models/serviceProvider.js
+++ models/serviceProvider.js
@@ -1,22 +1,26 @@
 const mongoose = require("mongoose");
 
 const serviceProviderSchema = new mongoose.Schema({
-    serviceProviderID:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User"
+  serviceProviderID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  categoryID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ServiceCategory",
+  },
+  hourlyCharge: {
+    type: String,
+  },
+  certificates: [
+    {
+      certificates: { type: String },
     },
-    categoryID:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"ServiceCategory"
-    },
-    certificates:[{
-        certificates:{type:String,}   
-     }],
-     isApprovedStatus:{
-      type:Boolean,
-      default:false
-     }
-
+  ],
+  isApprovedStatus: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 mongoose.model("ServiceProvider", serviceProviderSchema);
