770057366cb9d7e6f3a730b5376caf822fb5be4d
sachla <sach.pushpawela@gmail.com>
2022-06-28 11:57:40 +0530

    auth rollback

--- routes/auth.js
+++ routes/auth.js
@@ -95,7 +95,7 @@ router.post("/login", (req, res) => {
           const { _id, name } = savedUser;
           res.json({
             token,
-            user: { _id, email, name, city },
+            user: { _id, email, name },
           });
         } else {
           return res.status(422).json({ error: "Invalid Email or password" });
